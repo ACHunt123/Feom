e3d0d2e8f292dff7522adfdb776c70707319e4d3
Andrew Hunt <ach221@papageno>
2025-09-04 18:14:11 +0100

    minor refactoring and started the test sbeta file

--- SbetaTest.py
+++ SbetaTest.py
@@ -10,19 +10,22 @@ from Feom.setup import Setup
 from Feom.parser import params
 import matplotlib.pyplot as plt
 import numpy as np
-
+from Feom.baths.coth_decomp.cothAAA import get_coeffs
 
 ### Load all the parameters into the setup object 
 sim = Setup(params)
 
-# bath=sim.bath
+bath=sim.bath
+params=sim.params
+params.mu=params.K
 
-support=np.arange(-1000,1000,10000)
+support=np.arange(-100,100,0.01)
+fig,ax=plt.subplots()
+ax.plot(support,bath.J(support))
 
 
-# plt.plot(bath.J(support),support)
-
-# plt.show()
+get_coeffs(params, support, bath.J(support))
+plt.show()
 
 
 
--- baths/aaa/commands_to_run.m
+++ baths/aaa/commands_to_run.m
@@ -1,2 +1,2 @@
-run_aaa_fromfile(7,'/scratch2/ach221/data/Clsc/test/aaa_K7','/scratch2/ach221/data/Clsc/test/aaa_K7/aaa_data_quadrature_nw25001.txt','_quadrature_nw25001.txt',false)
+run_aaa_fromfile(7,'/scratch2/ach221/data/Clsc/test/aaa_K7','/scratch2/ach221/data/Clsc/test/aaa_K7/aaa_data_quadrature_nw20000.txt','_quadrature_nw20000.txt',false)
 run_aaa_fromfile(7,'/scratch2/ach221/data/Clsc/test/aaa_K7','/scratch2/ach221/data/Clsc/test/aaa_K7/aaa_data_quadrature_nw25001.txt','_quadrature_nw25001.txt',false)
